baebae83672fbb40eea6daa5b2a91bf83d09e6a1
AyanGhatak <ghatak.ayan.btech@gmail.com>
2016-08-19 10:05:43 +0530

    Add library files and show time in charts

--- 2016/js/domTest.js
+++ 2016/js/domTest.js
@@ -4,8 +4,7 @@ var svgNS = "http://www.w3.org/2000/svg",
 	buttonWrapper = document.getElementById("buttons"),
 	result = document.getElementById('result');  
 
-function createCircle(value)
-{	
+function createCircle(value){	
 	var hoverFN = function (e) {
 		var circle = this,
 			isMouseOver = e.type === 'mouseover';
@@ -27,9 +26,84 @@ function createCircle(value)
 		    circle.addEventListener("mouseout", hoverFN);
 		    container.appendChild(circle);
     	}
-    	result.innerHTML += 'It took ' + (performance.now() - time1).toFixed(2) + 'ms to create ' + value + ' no. of circles.<br />';
+    	updateResult.display(value, (performance.now() - time1).toFixed(2));
+    	// result.innerHTML += 'It took ' + (performance.now() - time1).toFixed(2) + 'ms to create ' + value + ' no. of circles.<br />';
     }
 } 
+var updateResult = (function () {
+	var obj = {},
+		JSONData = {
+		    "chart": {
+		        "caption": "Time taken to render the circles",
+		        "xaxisname": "Number of circles",
+		        "yaxisname": "Time taken in ms",
+		        "theme": "fint"
+		    },
+		    "categories": [
+		        {
+		            "category": []
+		        }
+		    ],
+		    "dataset": [
+		        {
+		            "data": []
+		        }
+		    ]
+		},
+		chartRef,
+		disp = function () {
+			var prop,
+				data = JSONData.dataset[0].data,
+				category = JSONData.categories[0].category;
+			data.length = 0;
+			category.length = 0;
+			for (prop in obj) {
+				category.push({
+					label: prop
+				});
+				data.push({
+					value: obj[prop]
+				});
+			}
+			if (chartRef) {
+				chartRef.setJSONData(JSONData);
+			}
+			else {
+				chartRef = new FusionCharts({
+			        type: 'msline',
+			        // type: 'logmsline',
+			        renderAt: 'chartContainer',
+			        width: '625',
+			        height: '350',
+			        dataFormat: 'json',
+			        dataSource: JSONData
+			    }).render();
+			}
+		},
+		changeChartType = function (isLogarithmic) {
+			if (isLogarithmic) {
+				chartRef.chartType('logmsline');
+			}
+			else {
+				chartRef.chartType('msline');	
+			}
+		};
+	return {
+		display: function (key, value) {
+			obj[key] = value;
+			disp();
+		},
+		changeChartType: changeChartType
+	}
+	return 
+})();
+
+(function () {
+	var toogleButton = document.getElementsByName('type')[0];
+	toogleButton.addEventListener('click', function () {
+		updateResult.changeChartType(this.checked);
+	})
+})()
 
 function createButton(value){
     var button = document.createElement("input");
@@ -52,4 +126,4 @@ function init(arr) {
 		createButton(arr[i]);
 	}
 }
-init([10, 100, 1000, 10000, 100000, 200000]);
+init([10, 100, 1000, 10000, 100000, 1000000]);
